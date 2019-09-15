e8b1d875c76c8feb9bb14af2719b1a880ce5252c
Selamettin Dirik <selamettinn.95@gmail.com>
2019-09-15 16:34:08 +0300

    Home Page is added

---
+++ paperdbhomepage.php
@@ -0,0 +1,10 @@
+<html>
+    <head>
+        <title>PaperDB</title>
+    </head>
+    <body>
+        <p>Welcome PaperDB</p>      
+        <?php echo "<br/><a href = 'paperadmin/home.php'>Login As Admin</a> <br/>"; ?>
+        <?php echo "<br/><a href = 'paperuser/home.php'>Login As User</a><br/>"; ?>
+    </body>
+</html>
